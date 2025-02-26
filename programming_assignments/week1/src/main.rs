2fc3b2428a0fd37fb5c1bdf5a8bccc0fadc7fa0e
tim-yiu <timyiutyty@gmail.com>
2025-02-27 00:05:22 +0800

    update

--- programming_assignments/week1/src/main.rs
+++ programming_assignments/week1/src/main.rs
@@ -9,16 +9,17 @@ fn main() {
     let mut cipher_texts = Vec::new();
     let target_cipher = fs::read_to_string(decrypt_target_file_path)
         .expect("Should have been able to read the file").into_bytes();
+    let target_text: Vec<u8> = Vec::new();
 
     for line in fs::read_to_string(cipher_text_file_path).unwrap().lines() {
         cipher_texts.push(line.to_string().into_bytes());
     }
 
-    for cipher in cipher_texts.iter() {
-        let cipher_str = str::from_utf8(&cipher).unwrap();
-        println!("{cipher_str}");
+    for target_byte in target_cipher {
+        for cipher in cipher_texts.iter() {
+            for byte in cipher {
+                println!("{byte}");
+            }
+        }
     }
-    
-    let target_cipher_str = str::from_utf8(&target_cipher).unwrap();
-    println!("{target_cipher_str}");
 }
