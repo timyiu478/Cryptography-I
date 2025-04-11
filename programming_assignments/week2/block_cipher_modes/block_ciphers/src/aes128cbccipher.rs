78481e743e9f29d33bdedf9668c6040aad1b0d49
tim <ctom3560@gmail.com>
2025-04-11 23:17:12 +0800

    add CBC

--- programming_assignments/week2/block_cipher_modes/block_ciphers/src/aes128cbccipher.rs
+++ programming_assignments/week2/block_cipher_modes/block_ciphers/src/aes128cbccipher.rs
@@ -1,6 +1,6 @@
 use crate::BlockCipher;
 use aes::Aes128;
-use cipher::{generic_array::GenericArray, BlockEncrypt, KeyInit};
+use cipher::{generic_array::GenericArray, BlockEncrypt, BlockDecrypt, KeyInit};
 use generic_array::typenum::U16;
 use rand::Rng;
 
@@ -22,14 +22,29 @@ impl Aes128CbcCipher {
     // Pad the plaintext by PKCS5 padding method
     //
     // Ref: https://www.cryptosys.net/pki/manpki/pki_paddingschemes.html
-    fn pkcs5Padding(plaintext: &[u8]) -> Vec<u8> {
+    fn pkcs5Padding(plaintext: Vec<u8>) -> Vec<u8> {
         let blockSize = 16;
-        let mut plaintextWithPadding: Vec<u8> = plaintext.to_vec();
+        let mut plaintextWithPadding: Vec<u8> = plaintext.clone();
         let paddingLength = blockSize - (plaintext.len() % blockSize);
 
         plaintextWithPadding.extend(vec![paddingLength as u8; paddingLength]);
 
         plaintextWithPadding
+    }
+
+    fn pkcs5DePadding(plaintextWithPadding: Vec<u8>) -> Vec<u8> {
+        let mut plaintext: Vec<u8> = plaintextWithPadding.clone();
+        let paddingLength: usize = usize::from(*plaintext.last().unwrap());
+
+        for i in 1..paddingLength {
+            if plaintextWithPadding[plaintext.len() - 1 - i] != *plaintext.last().unwrap() {
+                panic!("De padding error");
+            }
+        }
+
+        plaintext.truncate(plaintext.len() - paddingLength);
+
+        plaintext
     }
 
     fn xor_generic_arrays(
@@ -46,7 +61,7 @@ impl Aes128CbcCipher {
 }
 
 impl BlockCipher for Aes128CbcCipher {
-    fn encrypt(&self, plaintext: &[u8]) -> Vec<u8> {
+    fn encrypt(&self, plaintext: Vec<u8>) -> Vec<u8> {
         let iv = Aes128CbcCipher::generate_iv();
         let plaintextWithPadding = Aes128CbcCipher::pkcs5Padding(plaintext);
 
@@ -54,21 +69,17 @@ impl BlockCipher for Aes128CbcCipher {
         let mut cipherBlocks: Vec<GenericArray<u8, U16>> = Vec::new();
         let mut inBlock: GenericArray<u8, U16> = GenericArray::default();
         let mut outBlock: GenericArray<u8, U16> = GenericArray::default();
+        let mut encyptedIV = iv;
 
-        cipherBlocks.push(iv);
+        // Don't use in PROD; the IV should be encrypted by a seperate key to prevent chosen plaintext attack
+        cipher.encrypt_block(&mut encyptedIV);
+        cipherBlocks.push(encyptedIV);
 
         for (index, blockVec) in plaintextWithPadding.chunks(16).enumerate() {
             let block: GenericArray<u8, U16> = GenericArray::clone_from_slice(blockVec);
 
-            if index == 0 {
-                let mut encyptedIV = iv;
-                cipher.encrypt_block(&mut encyptedIV);
-
-                inBlock = Aes128CbcCipher::xor_generic_arrays(encyptedIV, block);
-            } else {
-                let previousBlock = cipherBlocks.last().expect("No previous block");
-                inBlock = Aes128CbcCipher::xor_generic_arrays(*previousBlock, block);
-            }
+            let previousBlock = cipherBlocks.last().expect("No previous block");
+            inBlock = Aes128CbcCipher::xor_generic_arrays(*previousBlock, block);
 
             cipher.encrypt_block_b2b(&inBlock, &mut outBlock);
 
@@ -84,8 +95,38 @@ impl BlockCipher for Aes128CbcCipher {
         ciphertext
     }
 
-    fn decrypt(&self, ciphertext: &[u8]) -> Vec<u8> {
-        let plaintext: Vec<u8> = Vec::new();
+    // The 16-byte encryption IV is chosen at random and is prepended to the ciphertext
+    fn decrypt(&self, ciphertext: Vec<u8>) -> Vec<u8> {
+        let cipher = Aes128::new(&self.key);
+
+        let mut plaintext: Vec<u8> = Vec::new();
+        let mut plainBlocks: Vec<GenericArray<u8, U16>> = Vec::new();
+        let mut inBlock: GenericArray<u8, U16> = GenericArray::default();
+        let mut outBlock: GenericArray<u8, U16> = GenericArray::default();
+        let mut prevBlock: GenericArray<u8, U16> = GenericArray::default();
+
+        for (index, blockVec) in ciphertext.chunks(16).enumerate() {
+
+            inBlock = GenericArray::clone_from_slice(blockVec);
+
+            if index == 0 { 
+                // get the encrypted IV
+                prevBlock = inBlock;
+                continue;
+            }
+
+            cipher.decrypt_block_b2b(&inBlock, &mut outBlock);
+
+            plainBlocks.push(Aes128CbcCipher::xor_generic_arrays(prevBlock, outBlock));
+
+            prevBlock = inBlock;
+        }
+
+        for plainBlock in plainBlocks {
+            plaintext.extend(plainBlock.iter().copied());
+        }
+        
+        plaintext = Aes128CbcCipher::pkcs5DePadding(plaintext);
 
         plaintext
     }
--- programming_assignments/week2/block_cipher_modes/src/main.rs
+++ programming_assignments/week2/block_cipher_modes/src/main.rs
@@ -1,13 +1,55 @@
 use block_ciphers::aes128cbccipher::Aes128CbcCipher;
 use block_ciphers::BlockCipher;
 use cipher::generic_array::GenericArray;
+use generic_array::typenum::U16;
+
+
+fn hex_to_vec(hex: &str) -> Vec<u8> {
+    let mut bytes = Vec::new();
+
+    // Iterate over the hex string in chunks of 2 characters
+    for chunk in hex.as_bytes().chunks(2) {
+        // Convert the chunk into a string slice
+        let pair = std::str::from_utf8(chunk).expect("Invalid UTF-8");
+
+        // Parse the hex pair into a u8
+        let byte = u8::from_str_radix(pair, 16).expect("Invalid hex");
+        bytes.push(byte);
+    }
+
+    bytes
+}
+fn hex_to_16_bytes_array(hex: &str) -> GenericArray<u8, U16> {
+    let bytes = hex_to_vec(hex);
+
+    GenericArray::from_exact_iter(bytes).expect("Conversion failed due to mismatched size")
+}
 
 fn main() {
     let key = GenericArray::from([0u8; 16]);
-    let plaintext = b"Hello, AES CBC Cipher!"; // Example plaintext
+    let plaintext = b"Hello, AES CBC Cipher!";
 
     let cipher = Aes128CbcCipher::new(key);
-    let ciphertext = cipher.encrypt(plaintext);
+    let ciphertext = cipher.encrypt(plaintext.to_vec());
+    let decryptedtext = cipher.decrypt(ciphertext.clone());
+    let readable = String::from_utf8_lossy(&decryptedtext);
 
+    println!("Plaintext: {:?}", plaintext);
     println!("Ciphertext: {:?}", ciphertext);
+    println!("Decryptedtext: {:?}", decryptedtext);
+    println!("Readable Decryptedtext: {:?}", readable);
+
+    // Q1
+    let q1_cbc_key: GenericArray<u8, U16> = hex_to_16_bytes_array("140b41b22a29beb4061bda66b6747e14");
+    let q1_ciphertext: Vec<u8> = hex_to_vec("4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81");
+    let q1_cipher = Aes128CbcCipher::new(q1_cbc_key);
+    println!("Q1: {:?}", String::from_utf8_lossy(&q1_cipher.decrypt(q1_ciphertext.clone())));
+
+    // Q2
+    let q2_cbc_key: GenericArray<u8, U16> = hex_to_16_bytes_array("140b41b22a29beb4061bda66b6747e14");
+    let q2_ciphertext: Vec<u8> = hex_to_vec("5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253");
+    let q2_cipher = Aes128CbcCipher::new(q2_cbc_key);
+    println!("Q2: {:?}", String::from_utf8_lossy(&q2_cipher.decrypt(q2_ciphertext.clone())));
+
+
 }
